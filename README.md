6117af8f73a71903238938538c480a71b408c4d6
David Lidström_c <david.lidstrom@sandvik.com>
2017-09-23 16:03:42 +0200

    First checkin

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gulp-sp-svida
-Gulp plugin for sp-svida
+
+This is a simple packaging of sp-svida (soon to be released properly).
+
+ * A simple plugin to wrap sp-svida functionality within a gulp task
+ * Includes sp-svida executables, dlls and requirements
+
+Return later for more details when this is more tested.
---
+++ index.js
@@ -0,0 +1,65 @@
+﻿var through = require('through2'),
+    gutil = require('gulp-util'),
+    spawn = require('child_process').spawn,
+    PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-sp-svida',
+    EXE_PATH = __dirname + 'bin\\sp-svida.exe', // bin later
+    filename = __dirname;
+
+var SPSvidaPlugin = (function () {
+
+
+    function preview() {
+
+        //gutil.log('stuff happened', 'Really it did', gutil.colors.magenta('123'));
+
+        function doit() {
+            return "";
+        }
+
+        return through.obj(function (file, encoding, callback) {
+
+
+            var parameters = [
+                'preview',
+                '/config="' + file.path + '"',
+                "/enterKey"
+                ],
+                child;
+
+            child = spawn(EXE_PATH, parameters);
+
+            child.on('exit', function (code, signal) {
+                gutil.log('Child process exited with ${code} and signal ${signal}');
+            });
+
+            child.stdout.on('data', (data) => {
+                gutil.log(`child stdout:\n${data}`);
+            });
+
+            child.stderr.on('data', (data) => {
+                gutil.log(`child stderr:\n${data}`);
+                child.kill();
+            });
+
+
+            gutil.log(EXE_PATH + ' preview /config="' + file.path + '"', 'Really it did', gutil.colors.magenta('123'));
+
+
+            callback(null, doit(file));
+        });
+
+
+    }
+
+    return {
+        preview : preview
+    }
+
+
+}());
+
+module.exports = {
+    preview: SPSvidaPlugin.preview
+};
